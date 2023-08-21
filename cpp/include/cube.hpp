b12bb50a7acd5ab8ccb4a4709ab07b10228410f6
JATothrim <jarmo.tiitto@gmail.com>
2023-08-21 18:01:38 +0300

    Provide configure option for the Cube struct compaction.
    - CUBES_PACK_CUBE_XYZ_ADDR CMake option.
      By default do still enable compaction of Cube struct into 8-bytes.
      If the hack does not work on some system this can be set to OFF
      to revert the hack on configure time.
    - Add assert into Cube::copyout()
    
    Signed-off-by: JATothrim <jarmo.tiitto@gmail.com>

--- cpp/include/cube.hpp
+++ cpp/include/cube.hpp
@@ -3,12 +3,13 @@
 #define OPENCUBES_CUBE_HPP
 
 #include <algorithm>
+#include <cassert>
 #include <cstdint>
 #include <memory>
 #include <unordered_set>
 #include <vector>
-#include <atomic>
 
+#include "config.hpp"
 #include "utils.hpp"
 
 struct XYZ {
@@ -49,44 +50,66 @@ struct Cube {
     // cube memory is stored two ways:
     // normal, new'd buffer: is_shared == false
     // shared, external memory: is_shared == true
-
+#if CUBES_PACK_CUBE_XYZ_ADDR == 1
     struct bits_t {
         uint64_t is_shared : 1;
         uint64_t size : 7;   // MAX 127
         uint64_t addr : 56;  // low 56-bits of memory address.
     };
+    static_assert(sizeof(bits_t) == sizeof(void *));
+#else
+    struct bits_t {
+        uint64_t addr;
+        uint8_t is_shared : 1;
+        uint8_t size : 7;  // MAX 127
+    };
+#endif
     // fields
     bits_t fields;
-
-    static_assert(sizeof(bits_t) == sizeof(void*));
     // extract the pointer from bits_t
     static XYZ *get(bits_t key) {
         // pointer bit-hacking:
         uint64_t addr = key.addr;
+#if CUBES_PACK_CUBE_XYZ_ADDR == 1
+// todo: on x86-64 depending if 5-level-paging is enabled
+// either 47-bit or 56-bit should be replicated to the high
+// part of the address. Don't know how to do this check yet,
+// so the high 8-bits is left zeroed.
+// If we get segfaults dereferencing get(fields)
+// then CUBES_PACK_CUBE_XYZ_ADDR must be disabled.
+#endif
         return reinterpret_cast<XYZ *>(addr);
     }
 
     static bits_t put(bool is_shared, int size, XYZ *addr) {
-        // mask off top byte from the memory address to fit it into bits_t::addr
+#if CUBES_PACK_CUBE_XYZ_ADDR == 1
+        // pack the memory address into 56-bits
         // on x86-64 it is not used by the hardware (yet).
         // This hack actually saves 8 bytes because previously
         // the uint8_t caused padding to 16 bytes.
-        // @note if we get segfaults dereferencing get(fields)
-        // then this is the problem and this hack must be undone.
         uint64_t tmp = reinterpret_cast<uint64_t>((void *)addr);
+        assert((tmp & ~0xffffffffffffff) == 0 && "BUG: CUBES_PACK_CUBE_XYZ_ADDR should be disabled");
         tmp &= 0xffffffffffffff;
         bits_t bits;
         bits.addr = tmp;
         bits.is_shared = is_shared;
         bits.size = size;
         return bits;
+#else
+        bits_t bits;
+        bits.addr = reinterpret_cast<uint64_t>((void *)addr);
+        bits.is_shared = is_shared;
+        bits.size = size;
+        return bits;
+#endif
     }
+
    public:
     // Empty cube
     Cube() : fields{put(0, 0, nullptr)} {}
 
     // Cube with N capacity
-    explicit Cube(uint8_t N) : fields{put(0,N, new XYZ[N])} {}
+    explicit Cube(uint8_t N) : fields{put(0, N, new XYZ[N])} {}
 
     // Construct from pieces
     Cube(std::initializer_list<XYZ> il) : Cube(il.size()) { std::copy(il.begin(), il.end(), begin()); }
@@ -97,7 +120,7 @@ struct Cube {
     // Construct from external source.
     // Cube shares this the memory until modified.
     // Caller guarantees the memory given will live longer than *this
-    Cube(const XYZ *start, uint8_t n) : fields{put(1,n,const_cast<XYZ*>(start))} {}
+    Cube(const XYZ *start, uint8_t n) : fields{put(1, n, const_cast<XYZ *>(start))} {}
 
     // Copy ctor.
     Cube(const Cube &copy) : Cube(copy.size()) { std::copy(copy.begin(), copy.end(), begin()); }
@@ -131,13 +154,9 @@ struct Cube {
 
     size_t size() const { return fields.size; }
 
-    XYZ *data() {
-		return get(fields);
-	}
+    XYZ *data() { return get(fields); }
 
-	const XYZ *data() const {
-		return get(fields);
-	}
+    const XYZ *data() const { return get(fields); }
 
     XYZ *begin() { return data(); }
 
@@ -169,12 +188,15 @@ struct Cube {
     /**
      * Copy cube data into destination buffer.
      */
-    void copyout(int num, XYZ* dest) const {
+    void copyout(int num, XYZ *dest) const {
+        assert(num <= size());
         std::copy_n(begin(), num, dest);
     }
 };
 
+#if CUBES_PACK_CUBE_XYZ_ADDR == 1
 static_assert(sizeof(Cube) == 8, "Unexpected sizeof(Cube) for Cube");
+#endif
 static_assert(std::is_move_assignable_v<Cube>, "Cube must be moveable");
 static_assert(std::is_swappable_v<Cube>, "Cube must swappable");
 
